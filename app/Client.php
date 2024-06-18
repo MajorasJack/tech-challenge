28ec1662ba1ffbbf0971c505122bf25ffcd293b8
MajorasJack <jack@gengar.me>
2024-06-18 16:08:38 +0100

    Updating sorting for Client Bookings
    * The default sorting was for the `id` ascending, I wasn't 100% clear on whether chronologically meant by the start date or the creation date
    
    * Creation date and ID will likely always be in the same order so I assumed it was implied to be the start time and this felt more useful for the end user
    
    Resolved:
    (FEATURE) The client bookings are currently displayed in random order. Please display them in chronological order, newest first.

--- app/Client.php
+++ app/Client.php
@@ -22,7 +22,7 @@ class Client extends Model
 
     public function bookings()
     {
-        return $this->hasMany(Booking::class);
+        return $this->hasMany(Booking::class)->orderBy('start');
     }
 
     public function user(): BelongsTo
