8dc03eaf88f59d9850f10bff38792125aea978a1
MajorasJack <jack@gengar.me>
2024-06-19 11:47:19 +0100

    Add testcase to ensure that the booking dates display in the right format

--- tests/Feature/Controllers/ClientsControllerTest.php
+++ tests/Feature/Controllers/ClientsControllerTest.php
@@ -20,7 +20,18 @@ class ClientsControllerTest extends TestCase
             ->assertOk()
             ->assertSee($booking->id)
             ->assertSee($booking->notes)
-            ->assertSee($booking->client_id)
+            ->assertSee($booking->client_id);
+    }
+
+    public function testItWillDisplayTheClientBookingDateInTheCorrectFormat()
+    {
+        $client = factory(Client::class)->create();
+
+        $booking = factory(Booking::class)->create(['client_id' => $client->id]);
+
+        $this->actingAs(factory(User::class)->create())
+            ->get(route('clients.show', $client))
+            ->assertOk()
             ->assertSee($booking->start->format('l d F o, G:i'))
             ->assertSee($booking->end->format('l d F o, G:i'));
     }
